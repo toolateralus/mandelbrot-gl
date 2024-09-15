b435c45d95ddfd85a97486acd2eed0e19e6c4718
toolateralus <joshnjoshgaming@gmail.com>
2024-09-15 19:05:17 -0400

    update to use new library functions

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main() {
   window.setClearColor(glm::vec4(0, 0, 0, 1));
 
   window.resizeEvent().subscribe([&](int x, int y) {
-    fontRenderer.setViewport({x,y});
+    fontRenderer.setViewport({x, y});
   });
 
   // Initialize framebuffer texture
@@ -66,21 +66,20 @@ int main() {
   float zoom = 1.0f;
 
   glEnable(GL_ALPHA_TEST);
+  glAlphaFunc(GL_BLEND, 0.5f);
   glDisable(GL_DEPTH_TEST);
 
-  while (!glfwWindowShouldClose(window.window)) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+  window.run([&] {
     const int samplesPerAxis = 1;
     const int samples = samplesPerAxis * samplesPerAxis;
     glm::vec2 offsets[32];
-    
-    // Generate offsets based on the number of samples per axis
+
     for (int i = 0; i < samplesPerAxis; i++) {
       for (int j = 0; j < samplesPerAxis; j++) {
-        offsets[i * samplesPerAxis + j] = glm::vec2(float(i) / float(samplesPerAxis), float(j) / float(samplesPerAxis));
+        offsets[i * samplesPerAxis + j] = glm::vec2(
+            float(i) / float(samplesPerAxis), float(j) / float(samplesPerAxis));
       }
-    } 
+    }
 
     auto transform = glm::dmat4(1.0);
     // apply pan and zoom
@@ -88,53 +87,61 @@ int main() {
     transform = glm::scale(transform, glm::dvec3(1 / zoom, 1 / zoom, 1));
     // convert screen space to ndc
     transform = glm::translate(transform, glm::dvec3(-1, -1, 0));
-    transform = glm::scale(transform, glm::dvec3(2, 2, 1) / glm::dvec3(window.resolution, 1));
-
-    computeShader.use();
-    computeShader.setVec2("resolution", window.resolution);
-    computeShader.setVec2("offsets", offsets[0], 32);
-    computeShader.setDMat4("transform", transform);
-    computeShader.setInt("maxIterations", 100 * (glm::log(zoom) + 1));
-    computeShader.setInt("samples", samples);
-
-    glBindImageTexture(1, framebufferTexture, 0, GL_FALSE, 0, GL_WRITE_ONLY,
-                       GL_RGBA32F);
-    glDispatchCompute(window.resolution.x / 16, window.resolution.y / 16, 1);
-    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-
-    static int lastFrameTime = 0;
-    fullscreenQuad.draw(framebufferTexture, "outputTexture");
-    fontRenderer.renderText(std::format("{} fps", lastFrameTime - glfwGetTime()), {0, 0}, 1, glm::vec4(1));
-    lastFrameTime = glfwGetTime();
-    glFinish();
+    transform = glm::scale(transform, glm::dvec3(2, 2, 1) /
+                                          glm::dvec3(window.resolution, 1));
     
-    glfwSwapBuffers(window.window);
-    glfwPollEvents();
+    // render
+    {
+      computeShader.use();
+      computeShader.setVec2("resolution", window.resolution);
+      computeShader.setVec2("offsets", offsets[0], 32);
+      computeShader.setDMat4("transform", transform);
+      computeShader.setInt("maxIterations", 100 * (glm::log(zoom) + 1));
+      computeShader.setInt("samples", samples);
 
-    if (Input::isKeyDown(GLFW_KEY_R)) {
-      Shader::hotReloadAll();
-      pan = {0, 0};
-      zoom = 1.0f;
+      glBindImageTexture(1, framebufferTexture, 0, GL_FALSE, 0, GL_WRITE_ONLY,
+                         GL_RGBA32F);
+      glDispatchCompute(window.resolution.x / 16, window.resolution.y / 16, 1);
+      glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+      static int lastFrameTime = 0;
+      fullscreenQuad.draw(framebufferTexture, "outputTexture");
+      fontRenderer.renderText(
+          std::format("{:.1f} fps", lastFrameTime - glfwGetTime()), {0, 0}, 1,
+          glm::vec4(1));
+      lastFrameTime = glfwGetTime();
+
+      glFinish();
     }
+    
+    // take inputs
+    {
+      if (Input::isKeyDown(GLFW_KEY_R)) {
+        Shader::hotReloadAll();
+        pan = {0, 0};
+        zoom = 1.0f;
+      }
 
-    static auto lastMousePos = Input::getMousePos();
-    float sensitivity = 0.001f;
+      static auto lastMousePos = Input::getMousePos();
+      float sensitivity = 0.001f;
 
-    if (Input::isButtonDown(GLFW_MOUSE_BUTTON_1)) {
-      auto pos = Input::getMousePos();
-      auto delta = (lastMousePos - pos) * sensitivity / zoom;
-      pan.x += delta.x;
-      pan.y -= delta.y;
-      lastMousePos = pos;
-    } else {
-      lastMousePos = Input::getMousePos();
+      if (Input::isButtonDown(GLFW_MOUSE_BUTTON_1)) {
+        auto pos = Input::getMousePos();
+        auto delta = (lastMousePos - pos) * sensitivity / zoom;
+        pan.x += delta.x;
+        pan.y -= delta.y;
+        lastMousePos = pos;
+      } else {
+        lastMousePos = Input::getMousePos();
+      }
+
+      auto scrollDelta = Input::scrollDelta();
+
+      if (scrollDelta.length() >= 0.1) {
+        zoom *= (1.0f + scrollDelta.y * 0.1f);
+      }
     }
+  });
 
-    auto scrollDelta = Input::scrollDelta();
-
-    if (scrollDelta.length() >= 0.1) {
-      zoom *= (1.0f + scrollDelta.y * 0.1f);
-    }
-  }
   glDeleteTextures(1, &framebufferTexture);
 }
